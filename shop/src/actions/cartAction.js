fb9ecfb15812537c080259f3ca80e98cf1873f91
Lee-ji-soo <nbsp3@naver.com>
2020-12-01 22:16:27 +0900

    [Cart.js] 수량 변경 시 수량 변경 확인 문구창 띄우기

--- shop/src/actions/cartAction.js
+++ shop/src/actions/cartAction.js
@@ -1,14 +1,18 @@
 const ON_ADD = 'ON_ADD';
 const ON_DELETE = 'ON_DELETE';
+const ON_CHANGE_QUANTITY = 'ON_CHANGE_QUANTITY';
 
 const onAdd = (item) => ({ type: ON_ADD, item });
 const onDelete = (id) => ({ type: ON_DELETE, id });
+const onChangeQuantity = (props) => ({ type: ON_CHANGE_QUANTITY, props })
 
 const cartAction = {
     ON_ADD,
     ON_DELETE,
+    ON_CHANGE_QUANTITY,
     onAdd,
-    onDelete
+    onDelete,
+    onChangeQuantity,
 };
 
 export { cartAction };
