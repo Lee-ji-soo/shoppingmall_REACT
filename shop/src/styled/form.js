92a464a93a1fe9cc2267b914dc67d0853033539c
Lee-ji-soo <nbsp3@naver.com>
2020-12-03 23:26:02 +0900

    [Input style] Input style 하나로 통일

--- shop/src/styled/form.js
+++ shop/src/styled/form.js
@@ -1,15 +1,33 @@
 import styled from 'styled-components';
-import { Basic, Button, Line } from './variables';
+import { Basic, Button, Input, Line } from './variables';
+
+const InputStyle = styled.div`
+    input[type=email],
+    input[type=password]{
+        display: block;
+        margin:${Line.margin};
+        width: 100%;
+        padding: ${Input.padding};
+        box-sizing: border-box;
+        border: ${Input.border};
+        border-radius:${Input.radius};
+        font-size: 1.1rem;
+        font-weight: bold;
+        text-align: center;
+        }
+`
 
 const FormStyle = styled.div`
     padding-top:${Basic.paddingTop};
     width:90%;  
     height:max-content;
     margin:0 auto;
+    .juso_wrap{
+    }
     .info_wrap{
         width:100%;
         padding-top:${Basic.midPaddingTop};
-        p{
+        div{
             width:100%;
             border-bottom:1px solid #eaeaea;
             .txt{
@@ -22,18 +40,6 @@ const FormStyle = styled.div`
                width:10%;
                min-width:130px;
                background-color:#f3f3f3;
-            }
-            button{
-                cursor:pointer;
-                padding:${Button.padding};
-                margin-left:30px;
-                border-radius:${Button.radius};
-                border:${Button.border};
-                background-color:${Button.backgroundColor};
-                &:hover{
-                    color:#fff;
-                    background-color:#333;
-                }
             }
         }
     }
@@ -59,4 +65,4 @@ const FormStyle = styled.div`
  }
 `
 
-export { FormStyle }
+export { FormStyle, InputStyle }
