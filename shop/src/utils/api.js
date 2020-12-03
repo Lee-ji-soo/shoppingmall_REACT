11e9c38f9c72906def1c6996f88c7b843babb4a8
Lee-ji-soo <nbsp3@naver.com>
2020-12-03 23:25:46 +0900

    [Mypage.js] 도로명 주소 API 셋팅

--- shop/src/utils/api.js
+++ shop/src/utils/api.js
@@ -1,10 +1,17 @@
+const REQ_URL = 'https://www.juso.go.kr/addrlink/addrLinkApi.do';
+const JUSO_CONFIRM_KEY = "devU01TX0FVVEgyMDIwMTIwMzIyMzkzMjExMDUwMDg=";
+
 const api = {
     fetchItems: async () => {
         const res = await
             fetch('../data/more.json');
-        await console.log(res);
         return await res.json();
     },
+    fetchJuso: async ({ page, keyword2 }) => {
+        const res = await
+            fetch(`${REQ_URL}?confmKey=${JUSO_CONFIRM_KEY}&currentPage=${page}&countPerPage=10&resultType=json&keyword=${keyword2}`);
+        return await res.json();
+    }
 }
 
 export { api };
